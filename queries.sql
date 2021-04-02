9679010c7d5c596110c7a65eae061878928397e9
Thomasjoe23 <thomasjoes23@gmail.com>
2021-04-01 20:04:34 -0500

    Finished Assignment 4!!!!!!

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 ## Part 1: Test it with SQL
+employer: varchar(255), id: int, name: varchar(255), skills: varchar(255)
 
 ## Part 2: Test it with SQL
+SELECT name from employer where location like '%St. Louis City%';
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT name, description FROM skills
+WHERE skill.id in (SELECT skills_id from job_skills)
+ORDER BY name;
